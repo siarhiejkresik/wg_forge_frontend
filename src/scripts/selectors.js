bb415e6d1d44b6e8e2fdb6375e8a777e17dc1600
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2019-03-10 00:03:13 +0300

    feat: add data selectors

---
+++ src/scripts/selectors.js
@@ -0,0 +1,33 @@
+export function getOrderById(data, id) {
+  return data.orders.byId[id];
+}
+
+export const getOrderValueByKey = key => (data, id) => {
+  return getOrderById(data, id)[key];
+};
+
+export function getUserById(data, id) {
+  return data.users.byId[id];
+}
+
+export function getUserByOrderId(data, id) {
+  const userId = getOrderById(data, id).user_id;
+  const user = getUserById(data, userId);
+  return user;
+}
+
+export function getCompanyById(data, id) {
+  return data.companies.byId[id];
+}
+
+export function getUserInfo(data, id) {
+  const user = getUserById(data, id);
+  const companyId = user.company_id;
+  const company = user.company_id === null ? null : getCompanyById(data, companyId);
+  return { user, company };
+}
+
+export function getUserInfoByOrderId(data, id) {
+  const user = getUserByOrderId(data, id);
+  return getUserInfo(data, user.id);
+}
