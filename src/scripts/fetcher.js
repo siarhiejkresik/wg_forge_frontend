5dda534af6d1fb53a825b3838dfdc29a1cb145f4
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2019-03-11 00:36:45 +0300

    refactor: fetcher

--- src/scripts/fetcher.js
+++ src/scripts/fetcher.js
@@ -1,12 +1,17 @@
+import normalizeData from './normalizer';
+
 import companies from '../../data/companies.json';
 import orders from '../../data/orders.json';
 import users from '../../data/users.json';
 
 const rawData = { companies, orders, users };
 
-function fetcher() {
-  // return Promise.all(rawData.map(data => Promise.resolve(data)));
+function getRawData() {
   return rawData;
+}
+
+function fetcher() {
+  return normalizeData(getRawData());
 }
 
 export default fetcher;
