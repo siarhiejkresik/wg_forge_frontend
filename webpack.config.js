3e01c2ce9f2171ad5f076d64a8c8132d8275f978
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2019-03-10 20:06:33 +0300

    build: add css loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,33 @@
-var express = require("express");
-var path = require("path");
-var HtmlWebpackPlugin = require("html-webpack-plugin");
+var express = require('express');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./src/app.js",
+  entry: './src/app.js',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js"
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html"
-    })
+      template: './src/index.html',
+    }),
   ],
+  devtool: 'source-map',
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
+    contentBase: path.join(__dirname, 'dist'),
     open: true,
     port: 9000,
     before: function(app, server) {
-      app.use("/api", express.static(path.join(__dirname, "data")));
-    }
-  }
+      app.use('/api', express.static(path.join(__dirname, 'data')));
+    },
+  },
 };
